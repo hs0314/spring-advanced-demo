862d481d5ee2368c5268b4e1d2194c4da02a5bd7
heesu <tedlee0314@gmail.com>
2024-02-25 22:54:47 +0900

    first commit'

---
+++ src/main/java/heesu/me/springadvanceddemo/SpringAdvancedDemoApplication.java
@@ -0,0 +1,13 @@
+package heesu.me.springadvanceddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdvancedDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/heesu/me/springadvanceddemo/v0/OrderRepositoryV0.java
@@ -0,0 +1,26 @@
+package heesu.me.springadvanceddemo.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId){
+        if(itemId.equals("ex")){
+            throw new IllegalStateException("exception occurs.");
+        }
+
+        // save
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try{
+            Thread.sleep(millis);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
